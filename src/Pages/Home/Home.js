3c07d9eefb0f28572ce23c6b32e6830a8b389172
farhanchy99 <farhanchowdhury1999@gmail.com>
2023-01-09 22:36:56 +0600

    farhan commit

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Survive_Prayer from '../Survive_Prayer/Survive_Prayer';
 
 const Home = () => {
     return (
         <div>
-            this is home
+            <Survive_Prayer></Survive_Prayer>
         </div>
     );
 };
