a20f41af987401b56155af2070fb36d8faba0a2a
Muhammad Hasan <75510932+hasansikdar@users.noreply.github.com>
2023-01-10 06:04:41 +0600

    Update Home.js

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,1 +1,12 @@
-Resolved
+import React from 'react';
+import DonationGallery from '../DonationGallery/DonationGallery';
+
+const Home = () => {
+    return (
+        <div className='bg-white'>
+            <DonationGallery></DonationGallery>
+        </div>
+    );
+};
+
+export default Home;
