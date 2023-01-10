7539836b1c6c7c5061aaf6e222b0a38725f72e24
maj-jinnah <maj.jinnah@gmail.com>
2023-01-10 11:19:12 +0600

    Jinnah is done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Survive_Prayer from '../Survive_Prayer/Survive_Prayer';
+import Top_Banner from '../Top_Banner/Top_Banner';
 
 const Home = () => {
     return (
         <div>
+            <Top_Banner></Top_Banner>
             <Survive_Prayer></Survive_Prayer>
         </div>
     );
