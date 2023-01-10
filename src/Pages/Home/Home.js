66c032791316400c91a24a332d10b4db06b56434
hasansikdar <hasansikdar973@gmail.com>
2023-01-10 08:49:24 +0600

    some changed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,1 +1,11 @@
-Resolved
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            this is home
+        </div>
+    );
+};
+
+export default Home;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,1 +1,11 @@
-Resolved
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Header;
